a3aad6ddbdaf0aacc88ef59f2e09f52184b5b35d
anderson361163 <anderson361163@gmail.com>
2020-12-15 01:12:52 -0300

    Atualizando

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class Conta
+    {
+        public double Saldo { get; set; }
+
+        public double CalcularTaxaAdministrativa()
+        {
+            return 1;
+        }
+    }
+
+    public class ContaCorrente : Conta
+    {
+        public double CalcularTaxaAdministrativa()
+        {
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.Saldo = 1000;
+            Console.WriteLine(contaCorrente.CalcularTaxaAdministrativa());
+            Console.ReadLine();
+        }
+    }
+}
